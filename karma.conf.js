cd8c887a815016c31f2f60893a0466d8ba9be213
Ron Apelbaum <ron.apelbaum@hpe.com>
2016-03-21 13:49:14 +0200

    setup coverage for tests

--- karma.conf.js
+++ karma.conf.js
@@ -28,13 +28,15 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'app/**/!(*.test).js': ['coverage']
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress','coverage'],
 
 
         // web server port
@@ -67,4 +69,4 @@ module.exports = function (config) {
         // how many browser should be started simultaneous
         concurrency: Infinity
     })
-}
+};
